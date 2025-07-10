308c11f1dc9a4756191d4bccdfd862f8b32c0bb2
Joshua Chiang <stevenchiang09@gmail.com>
2025-07-10 12:58:54 -0500

    adding analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 import { ThemeContextProvider } from "../components/navbar/ThemeContext"
 
 const geistSans = localFont({
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
       <ThemeContextProvider>
         {children}
       </ThemeContextProvider>
+      <Analytics />
       </body>
     </html>
   );
